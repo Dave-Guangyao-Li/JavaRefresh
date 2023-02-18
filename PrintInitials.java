626d44944b6881192f6520ba564b3d70c6eb85d3
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-02-18 12:08:09 -0800

    new challenge

---
+++ PrintInitials.java
@@ -0,0 +1,11 @@
+public class PrintInitials {
+    public static void main(String[] args) {
+        System.out.println("GGGGG  LLLLL");
+        System.out.println("SSSSS  AAAAA");
+        System.out.println("SSSSS  AAAAA");
+        System.out.println("SSSSS  AAAAA");
+        System.out.println("SSSSS  AAAAA");
+        System.out.println("SSSSS  AAAAA");
+        System.out.println("SSSSS  AAAAA");
+    }
+}
